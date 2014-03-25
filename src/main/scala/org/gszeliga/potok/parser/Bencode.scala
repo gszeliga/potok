8cb138286bec9f9e67c0ad437867d1c9f71f2e20
Guillermo Szeliga <gszeliga@gmail.com>
2014-03-25 20:16:13 +0800

    Improve Bencoding parser
    - create implicit from char to parser
    - create new combinator: delimitedBy
    - improve printing characters parser
    - improve test cases

--- src/main/scala/org/gszeliga/potok/parser/Bencode.scala
+++ src/main/scala/org/gszeliga/potok/parser/Bencode.scala
@@ -5,6 +5,13 @@ import scala.util.parsing.combinator.syntactical.StdTokenParsers
 import scala.util.parsing.combinator.ImplicitConversions
 import scala.util.parsing.input.Reader
 import scala.util.parsing.combinator._
+import java.net.URL
+import scala.util.parsing.input.CharArrayReader
+import scala.io.Source
+import java.io.FileReader
+import java.io.InputStreamReader
+import scala.util.parsing.input.StreamReader
+import java.io.FileInputStream
 
 trait BencodeType
 
@@ -15,26 +22,36 @@ case class BDict(get: Map[BString, BencodeType]) extends BencodeType
 
 class Parser extends RegexParsers {
 
-  def colon: Parser[Char] = elem(':') named ("colon")
+  implicit def fromCharToParser(ch: Char) = elem(ch)
+
   def natural: Parser[Int] = """\d+""".r ^^ (_.toInt) named ("natural")
   def signedInt: Parser[Int] = """(-){0,1}\d+""".r ^^ (_.toInt) named ("signed_int")
-  def literal: Parser[String] = """[\w\W\:\/\.]""".r named ("literal")
+  
+  //http://sanjaal.com/java/tag/java-regular-expression-posix-character-classes-us-ascii-only/
+  //www.regexplanet.com/advanced/java/index.html
+  def literal: Parser[String] = """[\p{Print}]""".r named ("printable characters")
+
+  def delimitedBy[A](left: Parser[Char], right: Parser[Char])(p: Parser[A]): Parser[A] = left ~> p <~ right
 
   def string: Parser[BString] =
-    natural ~ colon >> {
-      case size ~ c => repN(size, literal) ^^ (l => BString(l.mkString))
+    natural ~ ':' >> {
+      case size ~ _ => repN(size, literal) ^^ (l => BString(l.mkString))
     } named ("string")
 
   def int: Parser[BInt] = {
-    elem('i') ~> signedInt <~ elem('e') ^^ (BInt(_)) named ("int")
+    delimitedBy('i', 'e')(signedInt) ^^ (BInt(_)) named ("int")
   }
 
   def list: Parser[BList] = {
-    elem('l') ~> rep(string | int | list | dict) <~ elem('e') ^^ (BList(_)) named ("list")
+    delimitedBy('l', 'e') {
+      rep(string | int | list | dict)
+    } ^^ (BList(_)) named ("list")
   }
 
   def dict: Parser[BDict] = {
-    elem('d') ~> rep(string ~ (string | int | list | dict)) <~ elem('e') ^^ (_.map { case a ~ b => a -> b }) ^^ (l => BDict(l.toMap)) named ("dict")
+    delimitedBy('d', 'e') {
+      rep(string ~ (string | int | list | dict))
+    } ^^ (_.map { case key ~ value => key -> value }) ^^ (l => BDict(l.toMap)) named ("dict")
   }
 
   def root = string | int | list | dict
@@ -42,8 +59,13 @@ class Parser extends RegexParsers {
 }
 
 object Bencode extends Parser {
-  def parse(in: Reader[Char]): Option[BencodeType] = phrase(root)(in) match {
-    case Success(r, _) => Some(r)
-    case _ => None
+
+  class ParseError(val msg: String, val next: Input)
+
+  def parse(in: Reader[Char]): Either[BencodeType, ParseError] = phrase(root)(in) match {
+    case Success(r, _) => Left(r)
+    case NoSuccess(msg, next) => Right(new ParseError(msg, next))
   }
+
+  def parse(in: URL): Either[BencodeType, ParseError] = parse(StreamReader(new InputStreamReader(new FileInputStream(in.getFile()))))
 }
--- src/main/scala/org/gszeliga/potok/parser/test.sc
+++ src/main/scala/org/gszeliga/potok/parser/test.sc
@@ -5,21 +5,30 @@ import scala.io.Codec
 import java.io.FileInputStream
 import scala.collection.immutable.Stream
 import scala.util.parsing.input.CharArrayReader
+import scala.util.parsing.input.StreamReader
+import java.io.InputStreamReader
 
 object test {
 
-  val fis = new FileInputStream(getClass().getResource("/samples/The.Walking.Dead.S04E01.HDTV.x264-ASAP.eztv.torrent").getFile())
-                                                  //> fis  : java.io.FileInputStream = java.io.FileInputStream@3b362ca8
+  val fis = StreamReader(new InputStreamReader(new FileInputStream(getClass().getResource("/samples/torrent/The.Walking.Dead.S04E01.HDTV.x264-ASAP.eztv.torrent").getFile())))
+                                                  //> fis  : scala.util.parsing.input.StreamReader = scala.util.parsing.input.Stre
+                                                  //| amReader@5289cf1e
 
-	Stream.continually(fis.read).map(_.toByte.toChar)
-                                                  //> res0: scala.collection.immutable.Stream[Char] = Stream(d, ?)
- 
- 
- val aa = 0x75.toByte.toChar                      //> aa  : Char = u
- val bb = 0x31.toChar                             //> bb  : Char = 1
- new CharArrayReader("4:test".toCharArray())      //> res1: scala.util.parsing.input.CharArrayReader = scala.util.parsing.input.Ch
-                                                  //| arArrayReader@2d33886b
- 
- List(("Hola",1)).toMap                           //> res2: scala.collection.immutable.Map[String,Int] = Map(Hola -> 1)
- 
+  fis.first                                       //> res0: Char = d
+  fis.drop(1).first                               //> res1: Char = 8
+
+  (0 to 342).foldLeft((fis, "")) { (a, b) =>
+
+    val str = a._2 + a._1.first
+    (a._1.drop(1), str)
+
+  }                                               //> res2: (scala.util.parsing.input.StreamReader, String) = (scala.util.parsing.
+                                                  //| input.StreamReader@3f77b3cd,d8:announce36:udp://tracker.openbittorrent.com:8
+                                                  //| 0/13:announce-listll35:udp://tracker.openbittorrent.com:80el29:udp://tracker
+                                                  //| .publicbt.com:80el26:udp://tracker.istole.it:80el25:udp://open.demonii.com:8
+                                                  //| 0el32:udp://tracker.coppersurfer.tk:80el23:udp://tracker.ccc.de:80ee7:commen
+                                                  //| t56:Visit #EZTV on EFNet (irc.efnet.info) or http://eztv.it/13:crea)
+
+ List(" ", " ").mkString                          //> res3: String = "  "
+
 }
